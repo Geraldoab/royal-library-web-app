b38a5fe15261d7b6bba55394b36942744cb92def
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2024-05-17 06:23:59 -0300

    create a base component and apply inheritance

---
+++ royal-library/src/app/components/core/base-component/base.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-base-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './base.component.html',
+  styleUrl: './base.component.css'
+})
+export class BaseComponentComponent {
+
+  constructor(
+    protected router: Router,
+    protected snackBar: MatSnackBar
+  ) { }
+
+  openPage(routename: string) {
+    this.router.navigateByUrl(`/${routename}`);
+  }
+
+  showSnackBar(
+    message: string,
+    position: MatSnackBarVerticalPosition = 'bottom',
+    selectedDuration: number = 3000) {
+    this.snackBar.open(message, null, {
+      duration: selectedDuration,
+      verticalPosition: position
+    });
+  }
+}
